d9c000d55d3cefa3f1177195b2eb49164bb0be26
Thomas Eyaa <thomaseyaa.pro@gmail.com>
2025-06-17 03:24:26 +0200

    feat(layout): add Header component and updated app layout

---
+++ components/layout/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="w-full border-b-2 border-black-200">
+    <div class="p-4">
+      <div class="flex items-center justify-between">
+        <div class="flex-shrink-0">
+          <img
+            src="@/assets/img/logo.svg"
+            alt="Logo"
+            class="h-7 w-auto sm:h-10 md:h-12"
+          />
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
